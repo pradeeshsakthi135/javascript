9558f4c247d32d517ada99b79521c172c62cc765
Pradeesh <pradeeshsakthi135@gmail.com>
2025-10-24 20:03:56 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,35 @@
+let monthName = ["January"];
+console.log([monthName][0]);
+
+let bookTitle = ["fairy tales"];
+console.log([bookTitle][0]);
+
+let rollNumber = [234,456,987];
+console.log([rollNumber][0]);
+
+let dateOfBirth = [13,5,2002];
+console.log([dateOfBirth[0]]);
+
+let moneyBrought = [234,[786,987],[344]];
+console.log(moneyBrought[1][1]);
+
+let memberGender = ["male","Female"];
+console.log(memberGender[1]);
+
+let busNumber = [11,15,[34,84,29,21],[23,89,87]];
+console.log(busNumber[2][2]);
+
+let instituteName = ["sla"];
+console.log(instituteName[0]);
+
+let nameOfStudent = ["Pradeesh"];
+console.log(nameOfStudent[0])
+
+let findEvenNumbers = [13,15,17,19,22,33];
+console.log(findEvenNumbers[4]);
+
+let findOddNumbers = [22,44,66,88,[22,56]];
+console.log(findOddNumbers[4][1]);
+
+let fileName = ["HTML",[CSS],["JAVASCRIPT"]]
+console.log(fileName[1])
---
+++ object.js
@@ -0,0 +1,35 @@
+let collegeName = {name : "kongu Engineering college"};
+console.log(collegeName);
+
+let collegeAddress = {Address : "Erode"};
+console.log(collegeAddress);
+
+let departmentName = {department : "computer science and design"}
+console.log(departmentName);
+
+let collegeFees = [{payment1:"Cash",payment2:{payment3:"Gpay",payment4:"Paytm"}}];
+console.log(collegeFees[0].payment2);
+
+let payment_status = [{paymentmethod1:"paid",paymentmethod2:{paymentmethod3:"not paid",paymentmethod4:"paid"}}];
+console.log(payment_status[0].paymentmethod2);
+
+let collegeLocation= {location1:"chennai",location2:{location3:"Coimbatore",location4:"Erode"}};
+console.log(collegeLocation.location2.location3);
+
+let rollNumber = {rollNumber : "21CDR032"}
+console.log(rollNumber);
+
+let hostelFood ={menu : "veg and nonveg"};
+console.log(hostelFood);
+
+let college_Address = { address: "Perundurai,Erode"};
+console.log(college_Address);
+
+let collegeLanguage = {language : "English and tamil"};
+console.log(collegeLanguage);
+
+let departmentstudents = {students : "50"};
+console.log(departmentstudents);
+
+let studentName = {students : "Pradeesh"}
+console.log(studentName);
